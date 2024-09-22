e0892058d085bc70e81d607d5085d20d03400071
Shivam Sharma <meshivam81@gmail.com>
2024-09-22 17:30:37 +0530

    feat: Set up expensify client

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  const client = 'Shivam';
+  return (
+    <>
+      <div className="flex justify-center text-green-400">
+        <h1>Hello, {client}</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
