2e6190f98d873b95136afe58a69aecece54f93ec
Shivam Sharma <meshivam81@gmail.com>
2024-09-28 18:15:32 +0530

    refactor: NotFound page

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,15 +1,25 @@
-import { Link } from 'react-router-dom';
-
 const NotFound = () => {
   return (
-    <div className="flex justify-center">
-      <h1>404 Not Found</h1>
-      <p>Sorry, an error has occured, Requested page not found!</p>
-      <Link to="/" className="bg-blue-700 p-1">
-        Back to Home
-      </Link>
-    </div>
+    <section>
+      <div className=" text-white">
+        <div className="flex h-screen">
+          <div className="m-auto text-center">
+            <div>
+              <img src="/404.svg" alt="404" />
+            </div>
+            <p className="text-sm md:text-base text-[#F6009B] p-2 mb-4">
+              The stuff you were looking for doesn&apos;t exist
+            </p>
+            <a
+              href="/"
+              className="bg-transparent hover:bg-[#F6009B] text-[#F6009B] hover:text-white rounded shadow hover:shadow-lg py-2 px-4 border border-[#F6009B] hover:border-transparent"
+            >
+              Take me home
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
-
 export default NotFound;
