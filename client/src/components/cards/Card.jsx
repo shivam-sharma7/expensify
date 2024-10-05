205b270cb0071242edb37c44e9184b5179a55024
Shivam Sharma <meshivam81@gmail.com>
2024-10-05 19:25:12 +0530

    feat(Delete): User can delete transaction history

--- client/src/components/cards/Card.jsx
+++ client/src/components/cards/Card.jsx
@@ -8,6 +8,9 @@ import { HiPencilAlt } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
 import { ProfileImg } from '../../pages/Home';
 import { formatDate } from '../../utils/formatDate';
+import { useMutation } from '@apollo/client';
+import { toast } from 'react-hot-toast';
+import { DELETE_TRANSACTION } from '../../graphql/mutations/transaction.mutation';
 
 const categoryColorMap = {
   saving: 'from-green-700 to-green-400',
@@ -19,6 +22,9 @@ const categoryColorMap = {
 const Card = ({ transaction }) => {
   let { category, amount, location, date, paymentType, description } = transaction;
   const cardClass = categoryColorMap[category];
+  const [deleteTransaction, { loading }] = useMutation(DELETE_TRANSACTION, {
+    refetchQueries: ['GetTransactions'],
+  });
 
   description = description[0]?.toUpperCase() + description.slice(1);
   category = category[0]?.toUpperCase() + category.slice(1);
@@ -26,13 +32,27 @@ const Card = ({ transaction }) => {
 
   const formattedDate = formatDate(date);
 
+  const handleDelete = async () => {
+    try {
+      await deleteTransaction({
+        variables: {
+          transactionId: transaction._id,
+        },
+      });
+      toast.success('Transaction Deleted Successfully');
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   return (
     <div className={`rounded-md p-4 bg-gradient-to-br ${cardClass}`}>
       <div className="flex flex-col gap-3">
         <div className="flex flex-row items-center justify-between">
           <h2 className="text-lg font-bold text-white">{category}</h2>
           <div className="flex items-center gap-2">
-            <FaTrash className={'cursor-pointer'} />
+            {!loading && <FaTrash className={'cursor-pointer'} onClick={handleDelete} />}
+            {loading && <div className="font-semibold">Deleting</div>}
             <Link to={`/transaction/123`}>
               <HiPencilAlt className="cursor-pointer" size={20} />
             </Link>
