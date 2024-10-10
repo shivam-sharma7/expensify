9d7f7ae114601cb1f5752ae77b84af7cab44a6e2
Shivam Sharma <meshivam81@gmail.com>
2024-10-10 21:49:12 +0530

    Identify potential memory leaks and make get req in every 15 minutes

--- backend/cron.job.js
+++ backend/cron.job.js
@@ -3,7 +3,7 @@ import https from 'https';
 
 const URL = 'https://expensify-f43l.onrender.com';
 
-export const job = new cron.CronJob('*/10 * * * * *', function () {
+export const job = new cron.CronJob('*/15 * * * *', function () {
   https
     .get(URL, (res) => {
       if (res.statusCode === 200) {
@@ -15,4 +15,10 @@ export const job = new cron.CronJob('*/10 * * * * *', function () {
     .on('error', (e) => {
       console.error('Error while sending req', e);
     });
+
+  // Log memory usage
+  const memoryUsage = process.memoryUsage();
+  console.log(
+    `Memory Usage: RSS=${memoryUsage.rss}, HeapTotal=${memoryUsage.heapTotal}, HeapUsed=${memoryUsage.heapUsed}, External=${memoryUsage.external}`,
+  );
 });
